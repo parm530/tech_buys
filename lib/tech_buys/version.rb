b745af057b778902a4b2173d94ccc684097739a9
parm530 <parmsahadeo530@gmail.com>
2016-08-14 23:17:19 -0400

    Added a module called Createables that creates Laptop, Game and Wearable objects

--- lib/tech_buys/version.rb
+++ lib/tech_buys/version.rb
@@ -1,3 +1,14 @@
 module TechBuys
   VERSION = "0.1.0"
+  
+  module Createable
+
+    def create_techbuy(array)
+      array.collect do |hash|
+        self.new(hash)
+      end
+
+    end
+  end
+
 end
